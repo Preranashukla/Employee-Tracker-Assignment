4010adfbf38c5f1276f3d4f433961d1eb2345d46
lconnell1000 <lconnell1000@gmail.com>
2022-03-03 16:26:00 +1030

    seeds file completed

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT NOT NULL,
+manager_id INT,
 FOREIGN KEY (role_id)
 REFERENCES employee_role(id)
 ON DELETE CASCADE
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (department_name)
+VALUES 
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES
+("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 1200000, 2),
+("Account Manger", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4):
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Matt", "Dsenna", 1, null),
+("Huy", "Vo", 2, 1),
+("Lachlan", "Connell", 3, null),
+("Clayton", "Mcintosh", 4, 3),
+("Navpreet", "Ahuja", 5, null),
+("Prab", "Singh", 6, 5),
+("Michael", "Fabbro", 7, null),
+("Chantress", "Rabski", 8, 7);
